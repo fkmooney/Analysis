a02071ececa0174b2c2994d4e61babbc3e790be2
Kempton Mooney <fkmooney@users.noreply.github.com>
2022-10-07 09:52:55 -0400

    Add files via upload

---
+++ Weather_app/weather_pull.py
@@ -0,0 +1,38 @@
+from flask import Flask,  render_template
+
+import requests
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/',)
+def pull_weather():
+
+    try:
+        url = 'https://api.weather.gov/points/40.6722,-73.9668'
+        res = requests.get(url)
+        text = (res.json())
+        forecast_url = (text['properties']['forecast'])
+        forecastHourly_url = (text['properties']['forecastHourly'])
+
+        res = requests.get(forecast_url)
+        text = (res.json())
+        df = pd.DataFrame.from_dict(text['properties']['periods'])
+        df = df.drop(['number', 'temperatureUnit', 'icon'], axis=1)
+        forecast = df.to_html()
+
+        res = requests.get(forecastHourly_url)
+        text = (res.json())
+        dfh = pd.DataFrame.from_dict(text['properties']['periods'])
+        dfh = dfh.drop(['number', 'name','temperatureUnit', 'icon', 'detailedForecast'], axis=1)
+        forecastHourly = dfh.to_html()
+
+    except:
+        Weather_Summary = "not available"
+        Temperature = "not available"
+
+    return render_template("index.html",
+        forecast=forecast, 
+        forecastHourly=forecastHourly,
+        )
+    
